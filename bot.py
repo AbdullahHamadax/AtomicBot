fe0299e29dd9471bbc6b03dd3c7175e87c5c1108
WxlfS3npai <40581069+WxlfS3npai@users.noreply.github.com>
2018-07-01 23:44:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ import urllib.request
 from itertools import cycle
 client = commands.Bot(command_prefix= ',')
 status = ["dont forget to", "do ,help", "to see all the commands :)"]
-os.chdir(r'C:\Users\abdallah\Desktop\fot')
 client.remove_command('help')
 async def test():
     print('test')
@@ -40,59 +39,6 @@ async def on_ready():
    print("Logged in as " + client.user.name)
 
 
-@client.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
- 
-    await update_data(users, member)
- 
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
- 
- 
-@client.event
-async def on_message(message):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-
-    await update_data(users, message.author)
-    await add_experience(users, message.author, 5)
-    await level_up(users, message.author, message.channel)
-   
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
- 
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experience'] = 0
-        users[user.id]['level'] = 1
-        
-print("test")
-async def add_experience(users, user, exp):
-    users[user.id]['experience'] += exp
- 
-async def level_up(users, user, channel):
-    experience = users[user.id]['experience']
-    lvl_start = users[user.id]['level']
-    lvl_end = int(experience ** (1/4))
- 
-    if lvl_start < lvl_end:
-        await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
-        users[user.id]['level'] = lvl_end
-
-
-@client.command(pass_context=True)
-async def search(ctx, *args):
-    query = ""
-    for word in args:
-        query += word
-        query += " "
-    result = yt_search(query)
-    await client.say(result)
-(edited)
 @client.event
 async def on_message(message):
     await client.process_commands(message)
