b14350aabe733fa231a4ae99ae59c9ce9e5d6a0d
WxlfS3npai <40581069+WxlfS3npai@users.noreply.github.com>
2018-07-02 02:25:58 +0200

    Update AtomicBot.py

--- AtomicBot.py
+++ AtomicBot.py
@@ -11,8 +11,6 @@ import json
 import async_timeout
 import aiohttp
 import requests
-import urllib.parse
-import urllib.request
 from itertools import cycle
 client = commands.Bot(command_prefix= ',')
 status = ["dont forget to", "do ,help", "to see all the commands :)"]
@@ -37,50 +35,6 @@ async def change_status():
 @client.event
 async def on_ready():
    print("Logged in as " + client.user.name)
-
-
-@client.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
- 
-    await update_data(users, member)
- 
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
- 
- 
-@client.event
-async def on_message(message):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-
-    await update_data(users, message.author)
-    await add_experience(users, message.author, 5)
-    await level_up(users, message.author, message.channel)
-   
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
- 
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experience'] = 0
-        users[user.id]['level'] = 1
-        
-print("test")
-async def add_experience(users, user, exp):
-    users[user.id]['experience'] += exp
- 
-async def level_up(users, user, channel):
-    experience = users[user.id]['experience']
-    lvl_start = users[user.id]['level']
-    lvl_end = int(experience ** (1/4))
- 
-    if lvl_start < lvl_end:
-        await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
-        users[user.id]['level'] = lvl_end
 
 @client.command(pass_context=True)
 async def addrole(ctx, member: discord.Member, roles):
